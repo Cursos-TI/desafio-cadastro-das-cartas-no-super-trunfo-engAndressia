a18951b1444c3949743f1c1b078700909b8a6949
engAndressia <andressiaengenheira@gmail.com>
2025-09-06 18:59:51 +0000

    AdcicioneiNívelAventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-
     char estado1[20], estado2[20], codigocarta1[20], codigocarta2[20], cidade1[20], cidade2[20];
     int populacao1, populacao2, pturisticos1, pturisticos2;
-    float area1, area2, pib1, pib2; //variáveis no nível novato
-
+    float area1, area2, pib1, pib2, //Variáveis nível Novato
+    float densipopulacional1, densipopulacional2;;
+    float pibpercapita1, pibpercapita2; //variáveis no nível Aventureiro
     //informações carta 1 
 
     printf("Carta 1: \n");
@@ -25,8 +25,19 @@ int main(){
     printf("digite o número de pontos turísticos: \n");
     scanf("%d", &pturisticos1);
 
-    //informações carta 2
+    printf("Digite o número da população: \n");
+    scanf("%d", &populacao1);
+    printf("Digite o número da área: \n");
+    scanf("%f", &area1);
+    densipopulacional1 = populacao1 / area1; //Calcular Densidade Populacional NV
+    pibpercapita1 = pib1 / (float)populacao1; //Nível Aventureiro
 
+    //Sáida de dados da Densidade e Pib percapita
+
+    printf("A densidade Populacional é: %.2f", densipopulacional1);
+    printf("O pib percapita é: %.2f", pibpercapita1)
+
+    //informações carta 2
     printf("Carta 2: \n");
 
     printf("digite o estado: \n");
@@ -43,6 +54,9 @@ int main(){
     scanf("%f", &pib2);
     printf("digite o número de pontos turísticos: \n");
     scanf("%d", &pturisticos2);
+    densipopulacional2 = populacao2 / area2 //Nível Aventureiro
+    pibpercapita2 = pib2 / (float)populacao2 //NVba
+
 
     return 0;
 }
