594f6f7a78ce1ea09bc4064952c7b29373bbbb2b
engAndressia <andressiaengenheira@gmail.com>
2025-09-18 20:16:25 +0000

    úlima parte do jogo completa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@ int main(){
     int pturisticos1, pturisticos2;
     unsigned long int populacao1, populacao2; //Variáveis Nível Mestre
     float area1, area2, pib1, pib2, //Variáveis nível Novato
-    int densipopulacional;
+    float densipopulacional;
     float densipopulacional2;
     float pibpercapita1, pibpercapita2; //variáveis no nível Aventureiro
     float SuperPoder1, SuperPoder2; //Variáveis Nívem Mestre
@@ -67,16 +67,43 @@ int main(){
 
     //Resultado da Primeira Carta
 
-    printf("-----Primeira Carta. \n");
+    printf("-----Primeira Carta-----. \n");
     printf("A CARTA 1 É. \n");
     printf("Estado é: %s\n", estado1);
     printf("cidade é: %s\n", cidade1);
     printf("Código é: %s\n", codigocarta1);
     printf("População é: %d\n", populacao1)
     printf("Área é: %.2f\n", area1);
-    printf("Pib é: %.2f milhões\n");
+    printf("Pib é: %.2f milhões\n", pib1);
     printf("Pontos turísticos é: %d\n", pturisticos1);
-    printf("Densidade Populacional é: %.2f\n", densipopulacional1);
+    printf("Densidade Populacional é: %.2f\n", densipopulacional1); //Monstrar densidade populacional do Nívem Aventureiro
+    printf("Pib per capita é: %.2f\n", pibpercapita1); //Mostrar pib per capita do Nívem Aventureiro
+
+
+    printf("-----Segunda Carta-----. \n");
+    printf("A CARTA 1 É. \n");
+    printf("Estado é: %s\n", estado2);
+    printf("cidade é: %s\n", cidade2);
+    printf("Código é: %s\n", codigocarta2);
+    printf("População é: %d\n", populacao2)
+    printf("Área é: %.2f\n", area2);
+    printf("Pib é: %.2f milhões\n", pib2);
+    printf("Pontos turísticos é: %d\n", pturisticos2);
+    printf("Densidade Populacional é: %.2f\n", densipopulacional2); //Mostrar densidade populacional do Nível Aventureiro
+    printf("Pib per capita é: %.2f\n", pibpercapita2); //Mostrar Pib per capita do Nível Aventureiro
+
+    //Última parte Níel Mestre
+    //Comparar carta e Verificar a Vencedora
+
+    printf("------A CARTA VENDECORA É------: \n");
+    printf("População: \n carta 1: %d Carta 2: %d", (populacao1>populacao2), (populacao2>populacao1));
+    printf("Área: \n Carta 1:%.2f Carta 2: %.2f", (area1>area2), (area2>area1));
+    printf("Pib: \n Carta 1: %.2f Carta 2: %.2f", (pib1>pib2), (pib2>pib1));
+    printf("Pontos Turisticos: \n Carta 1: %d Carta 2: %d", (pturisticos1>pturisticos2), (pturisticos2>pturisticos1));
+    printf("Densidade Populacional: \n Carta1: %d Carta 2: %d", (densipopulacional1>densipopulacional2), (densipopulacional2>densipopulacional1));
+    printf("Pib Per Capita: \n Carta1: %d CArta 2: %d", (pibpercapita1>pibpercapita2), (pibpercapita2>pibpercapita1));
+    printf("Super Poder: \n Carta 1: %d Carta 2: %d", (SuperPoder1>SuperPoder2), (SuperPoder2>SuperPoder1));
+    printf(\n"Fim do Código"\n);
 
     return 0;
 }
